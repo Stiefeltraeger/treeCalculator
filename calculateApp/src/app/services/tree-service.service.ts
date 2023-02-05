7af76b42d89dc23e6b4ec8475397e71ff10f15e4
Stiefeltraeger <104917667+Stiefeltraeger@users.noreply.github.com>
2023-02-05 14:34:18 +0100

    ka

--- calculateApp/src/app/services/tree-service.service.ts
+++ calculateApp/src/app/services/tree-service.service.ts
@@ -30,4 +30,73 @@ export class TreeServiceService {
     nP.children.push(this.startingElem[0], nS)
     this.startingElem = [nP]
   }
+
+  public resetTree(branch: Branch): void{
+    branch.value = 0
+    branch.valueCalculated = false
+    for (let br of branch.children){
+      this.resetTree(br)
+    }
+  }
+
+  calculateValuesForAllBellow(branch: Branch): void{
+    if (true) {
+      if (branch.children.length === 0){
+        throw new Error('Cant calculate the branch because children are undefined')
+      }
+      let calculatedValues = []
+
+      // then a recursion to get all the uncalculated values
+      for (let uc of branch.children.filter((child) => !child.valueCalculated && child.usedInCalculation)){
+        this.calculateValuesForAllBellow(uc)
+        calculatedValues.push(uc.value)
+      }
+
+      //first all values that are calculated
+      for (let v of branch.children.filter((child) => child.valueCalculated && child.usedInCalculation)){
+        let value = v.value
+        if (!v.positive){
+          value = 6 - value
+        }
+        calculatedValues.push(value)
+      }
+
+
+
+      if (branch.logic === 'AND'){calculatedValues.sort((a,b) => {
+        if (a > b) return 1
+        if (a < b) return -1
+        else return 0
+      })
+        console.log(branch.name + calculatedValues)
+
+        branch.value = calculatedValues[0]
+        branch.valueCalculated = true;
+
+      }
+      else if (branch.logic === 'OR'){calculatedValues.sort((a,b) => {
+        if (a > b) return -1
+        if (a < b) return 1
+        else return 0
+      })
+        console.log(branch.name + calculatedValues)
+        branch.value = calculatedValues[0]
+        branch.valueCalculated = true;
+
+      }
+
+      else if (branch.name === 'Angriffsabsicht'){
+        let nerf = 0
+        if(branch.children[0].value === 1){
+          nerf = 2
+        }
+        else if (branch.children[0].value === 1){
+          nerf = 1
+        }
+        branch.value = branch.children[1].value - nerf
+        branch.valueCalculated = true
+      }
+    }
+  }
+
 }
